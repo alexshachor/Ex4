d779381c61840601ff6905b7df256d7ac0109016
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2019-06-17 21:39:06 +0300

    change style of login activity

--- app/src/main/java/com/example/ex4/LoginActivity.java
+++ app/src/main/java/com/example/ex4/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.ex4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void connect(View v) {
+        findViewById(R.id.LoginActivity).setBackgroundColor(Color.LTGRAY);
     }
 }
