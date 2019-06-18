12dc6e0c62fab2cc5bf6c8cac53562fe9f2b04d0
Alexander Shachor <alexshachor@gmail.com>
2019-06-18 14:01:18 +0300

    Add comments and prettify code

--- app/src/main/java/com/example/ex4/JoystickActivity.java
+++ app/src/main/java/com/example/ex4/JoystickActivity.java
@@ -10,6 +10,9 @@ public class JoystickActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joystick);
+
+        //TODO start joystick with 0,0 values instead of nan
+        //TODO remove this
         ClientHandler clientHandler = new ClientHandler();
         clientHandler.updateAileron(0.2);
         clientHandler.updateElevator(0.97);
@@ -18,6 +21,6 @@ public class JoystickActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-//TODO close connection on destroy, check if changing view to landscape view calls onDestroy
+    //TODO close connection on destroy, check if changing view to landscape view calls onDestroy
     }
 }
