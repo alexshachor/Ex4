df3cce8be805b7da5b9cec19aa1c467e5e2c2a8b
Alexander Shachor <alexshachor@gmail.com>
2019-06-18 17:32:55 +0300

    Fix onDestroy bug

--- app/src/main/java/com/example/ex4/JoystickActivity.java
+++ app/src/main/java/com/example/ex4/JoystickActivity.java
@@ -3,6 +3,7 @@ package com.example.ex4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class JoystickActivity extends AppCompatActivity {
 
@@ -10,17 +11,14 @@ public class JoystickActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joystick);
-
-        //TODO start joystick with 0,0 values instead of nan
-        //TODO remove this
-        ClientHandler clientHandler = new ClientHandler();
-        clientHandler.updateAileron(0.2);
-        clientHandler.updateElevator(0.97);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-    //TODO close connection on destroy, check if changing view to landscape view calls onDestroy
+        if (isFinishing()) {
+            ClientHandler clientHandler = new ClientHandler();
+            clientHandler.stopConnection();
+        }
     }
 }
